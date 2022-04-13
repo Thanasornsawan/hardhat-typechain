ee6f8a0f1005816c8a6a522b0766dbd2d6a93f3f
thanasornsawan <thanasornsawan.ploy@gmail.com>
2022-04-13 16:02:09 +0300

    edit deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,10 +4,17 @@ async function main() {
 
   const Greeter = await ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
-
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Greeter deployed to: ", greeter.address);
+  console.log("Greeter transaction hash: ",greeter.deployTransaction.hash);
+
+  const Counter = await ethers.getContractFactory("Counter");
+  const counter = await Counter.deploy();
+  await counter.deployed();
+
+  console.log("Counter deployed to: ", counter.address);
+  console.log("Counter transaction hash: ", counter.deployTransaction.hash);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
